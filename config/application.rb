ef26696da193511e0e2fed3ee04699784cd105c1
Bilanuk <bilanuk.bill@gmail.com>
2022-11-04 11:35:44 +0200

    remove default rails test generator/files

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ Bundler.require(*Rails.groups)
 
 module KnSoundRails
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework nil
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
