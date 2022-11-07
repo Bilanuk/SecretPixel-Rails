aa62018b1c65bd50ec59e66b2b97d509e80204af
Bilanuk <bilanuk.bill@gmail.com>
2022-11-07 13:46:22 +0200

    heroku fix 1

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     scope :v1 do
-      devise_for :users,
-                 controllers: {
-                   sessions:      "users/sessions",
-                   registrations: "users/registrations"
-                 }
+      # devise_for :users,
+      #            controllers: {
+      #              sessions:      "users/sessions",
+      #              registrations: "users/registrations"
+      #            }
       get "/member-data", to: "members#show" # just for test purpose
     end
   end
