4078304a8147dfc1fcc3c64da8f9477cbe2497bc
BiLaNuK <bilanuk.bill@gmail.com>
2023-05-28 11:29:20 +0300

    fresh initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,45 +8,5 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 User.destroy_all
-Track.destroy_all
-Playlist.destroy_all
 
 user = User.create(email: "romanbilanuk@gmail.com", username: "BiLaNuK", password: "12345678", password_confirmation: "12345678")
-track = Track.new(author: user, title: "track")
-track.track.attach(io: File.open('public/track.mp3'), filename: 'track.mp3', content_type: 'audio/mp3')
-track.save!
-
-#First playlist with songs
-playlist = Playlist.new(title: "ChillOut", author: user)
-playlist.image_cover.attach(io: File.open('public/chillout.jpg'), filename: 'chillout.jpg', content_type: 'image/jpg')
-playlist.save!
-track = Track.new(author: user, playlist: playlist, title: "Moon")
-track.track.attach(io: File.open('public/moon.mp3'), filename: 'moon.mp3', content_type: 'audio/mp3')
-track.image_cover.attach(io: File.open('public/moon.png'), filename: 'moon.png', content_type: 'image/png')
-track.save!
-track = Track.new(author: user, playlist: playlist, title: "Snowfall")
-track.track.attach(io: File.open('public/snowfall.mp3'), filename: 'snowfall.mp3', content_type: 'audio/mp3')
-track.save!
-track = Track.new(author: user, playlist: playlist, title: "Virgo")
-track.track.attach(io: File.open('public/Virgo.mp3'), filename: 'Virgo.mp3', content_type: 'audio/mp3')
-track.save!
-track = Track.new(author: user, playlist: playlist, title: "letter to jarad")
-track.track.attach(io: File.open('public/letter to jarad.mp3'), filename: 'letter to jarad.mp3', content_type: 'audio/mp3')
-track.save!
-
-
-#Second playlist with songs
-playlist = Playlist.new(title: "Hip-hop", author: user)
-playlist.image_cover.attach(io: File.open('public/6obby.jpg'), filename: '6obby.jpg', content_type: 'image/jpg')
-playlist.save!
-track = Track.new(author: user, playlist: playlist, title: "miss me")
-track.track.attach(io: File.open('public/6obby - miss me.mp3'), filename: '6obby - miss me.mp3', content_type: 'audio/mp3')
-track.save!
-track = Track.new(author: user, playlist: playlist, title: "feed me to the wolves")
-track.track.attach(io: File.open('public/6obby - feed me to the wolves.mp3'), filename: '6obby - feed me to the wolves.mp3', content_type: 'audio/mp3')
-track.save!
-track = Track.new(author: user, playlist: playlist, title: "Princess Bride")
-track.track.attach(io: File.open('public/40rtycal - Princess Bride.mp3'), filename: '40rtycal - Princess Bride.mp3', content_type: 'audio/mp3')
-track.save!
-
-
