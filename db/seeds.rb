8926b6decc7aa518c64e3443ded0f947d8aa59fe
Vladyslav Knysh <zamahaw@gmail.com>
2023-05-28 19:25:05 +0300

    add image seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@ user = User.create(email: "romanbilanuk@gmail.com", username: "BiLaNuK", passwor
 # Add images
 image1 = user.images.create(name: "Image 1")
 image1.file.attach(io: File.open("./public/moon.png"), filename: "moon.png")
+
+image2 = user.images.create(name: "Image 2")
+image2.file.attach(io: File.open("./public/moon.png"), filename: "moon.png")
+
+image3 = user.images.create(name: "Image 3")
+image3.file.attach(io: File.open("./public/moon.png"), filename: "moon.png")
+
+image4 = user.images.create(name: "Image 4")
+image4.file.attach(io: File.open("./public/moon.png"), filename: "moon.png")
